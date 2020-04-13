63d9afed46850b5393a102aed5b4e642dd8b4fb5
Wescley Matos <wescleymatos@gmail.com>
2020-04-13 19:40:33 -0300

    #29 - Resolução de conflito

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -8,19 +8,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-<<<<<<< HEAD
-import { GraficoBarChartComponent } from './grafico-bar-chart/grafico-bar-chart.component';
-import { GraficoPieComponent } from './grafico-pie/grafico-pie.component';
-import { CardQuantitativoPrincipalComponent } from './card-quantitativo-principal/card-quantitativo-principal.component';
-import { CardQuantitativoSecundarioComponent } from './card-quantitativo-secundario/card-quantitativo-secundario.component';
-import { IssueOverviewComponent } from './issue-overview/issue-overview.component';
-import { QuantidadeTelasOverviewComponent } from './quantidade-telas-overview/quantidade-telas-overview.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { IssuesScreenComponent } from './issues-screen/issues-screen.component';
+import { IssuesScreenComponent } from './components/issues-screen/issues-screen.component';
 import { IssuesComponent } from './pages/issues/issues.component';
-import { FilterIssuesComponent } from './filter-issues/filter-issues.component';
-import { IssuesTableComponent } from './issues-table/issues-table.component';
-=======
+import { FilterIssuesComponent } from './components/filter-issues/filter-issues.component';
+import { IssuesTableComponent } from './components/issues-table/issues-table.component';
 import { IssueOverviewComponent } from './pages/issue-overview/issue-overview.component';
 import { GraficoBarChartComponent } from './components/grafico-bar-chart/grafico-bar-chart.component';
 import { GraficoPieComponent } from './components/grafico-pie/grafico-pie.component';
@@ -29,7 +20,6 @@ import { CardQuantitativoSecundarioComponent } from './components/card-quantitat
 import { QuantidadeTelasOverviewComponent } from './components/quantidade-telas-overview/quantidade-telas-overview.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { QuantityByLabelsComponent } from './components/quantity-by-labels/quantity-by-labels.component';
->>>>>>> master
 
 @NgModule({
   declarations: [
@@ -43,15 +33,11 @@ import { QuantityByLabelsComponent } from './components/quantity-by-labels/quant
     IssueOverviewComponent,
     QuantidadeTelasOverviewComponent,
     NavBarComponent,
-<<<<<<< HEAD
     IssuesScreenComponent,
     IssuesComponent,
     FilterIssuesComponent,
-    IssuesTableComponent
-=======
+    IssuesTableComponent,
     QuantityByLabelsComponent
-
->>>>>>> master
   ],
   imports: [
     BrowserModule,
