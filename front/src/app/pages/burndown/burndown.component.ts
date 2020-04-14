1174176906e94410ad77cb1b33d39cb1252bd70e
PauloRenato98 <paulo__159@hotmail.com>
2020-04-14 00:41:55 -0300

    adicionando layout do burndown

--- front/src/app/pages/burndown/burndown.component.ts
+++ front/src/app/pages/burndown/burndown.component.ts
@@ -1,3 +1,4 @@
+import { ChartOptions } from 'chart.js';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./burndown.component.css']
 })
 export class BurndownComponent implements OnInit {
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      position: 'top',
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value, ctx) => {
+          const label = ctx.chart.data.labels[ctx.dataIndex];
+          return label;
+        },
+      },
+    }
+  };
+  public pieChartLabels = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartData: number[] = [300, 500, 100];
+  public pieChartType = 'pie';
+  public pieChartLegend = true;
+ 
+  public pieChartColors = [
+    {
+      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+    },
+  ];
+
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+ 
 
   constructor() { }
 
