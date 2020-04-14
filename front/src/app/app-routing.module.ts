ddcbf78df417eaf69c8cde1b18bd4f4a3cfeff8b
PauloRenato98 <paulo__159@hotmail.com>
2020-04-13 23:16:46 -0300

    add rota do burdown

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { IssuesComponent } from './pages/issues/issues.component';
+import { BurndownComponent } from './pages/burndown/burndown.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'issues', component: IssuesComponent },
-  { path: 'burndown', component: IssuesComponent },
+  { path: 'burndown', component: BurndownComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
-
 ];
 
 @NgModule({
