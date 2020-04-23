9ae82783a294da2076af43a5c47928561f70c3af
PauloRenato98 <paulo__159@hotmail.com>
2020-04-23 20:47:44 -0300

    #17 Melhorando component do gráfico

--- front/src/app/pages/burndown/burndown.component.ts
+++ front/src/app/pages/burndown/burndown.component.ts
@@ -35,11 +35,11 @@ export class BurndownComponent implements OnInit {
     Chart.defaults.global.defaultFontSize = 14;
 
     const totalHoursInSprint = burndownData[0];
-    const idealHoursPerDay = totalHoursInSprint / 9;
+    const idealHoursPerDay = totalHoursInSprint / 4;
     let i = 0;
 
     const speedData = {
-      labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9', 'Day 10'],
+      labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5'],
       datasets: [
         {
           label: 'Burndown',
@@ -62,11 +62,6 @@ export class BurndownComponent implements OnInit {
             Math.round(totalHoursInSprint - (idealHoursPerDay * i++) + this.sumArrayUpTo(scopeChange, 2)), // 3
             Math.round(totalHoursInSprint - (idealHoursPerDay * i++) + this.sumArrayUpTo(scopeChange, 3)), // 4
             Math.round(totalHoursInSprint - (idealHoursPerDay * i++) + this.sumArrayUpTo(scopeChange, 4)), // 5
-            Math.round(totalHoursInSprint - (idealHoursPerDay * i++) + this.sumArrayUpTo(scopeChange, 5)), // 6
-            Math.round(totalHoursInSprint - (idealHoursPerDay * i++) + this.sumArrayUpTo(scopeChange, 6)), // 7
-            Math.round(totalHoursInSprint - (idealHoursPerDay * i++) + this.sumArrayUpTo(scopeChange, 7)), // 8
-            Math.round(totalHoursInSprint - (idealHoursPerDay * i++) + this.sumArrayUpTo(scopeChange, 8)), // 9
-            Math.round(totalHoursInSprint - (idealHoursPerDay * i++) + this.sumArrayUpTo(scopeChange, 9))  // 10
           ]
         },
       ]
