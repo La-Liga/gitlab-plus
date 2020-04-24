64b0ff77300f821aff0ab12ea9069cfc8823851d
rodrigogoncalves123 <rodrigodaniel@cc.ci.ufpb.br>
2020-04-24 18:54:49 -0300

    #82-Correcao das guardas de rotas

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -10,14 +10,10 @@ import { AuthGuard } from './pages/auth-guard.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'issues', component: IssuesComponent },
-  { path: 'burndown', component: BurndownComponent },
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', canActivate: [AuthGuard], component: DashboardComponent },
   { path: 'issues', canActivate: [AuthGuard], component: IssuesComponent },
-  { path: 'burndown', canActivate: [AuthGuard], component: IssuesComponent },
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'burndown', canActivate: [AuthGuard], component: BurndownComponent },
+  { path: '', canActivate: [AuthGuard], redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', canActivate: [AuthGuard], component: LoginComponent }
 ];
 
