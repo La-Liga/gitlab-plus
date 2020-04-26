8c11f59ffbbe6d5bbb6667c2ac8463fd12951262
Wescley Matos <wescleymatos@gmail.com>
2020-04-26 19:28:00 -0300

    #88 - Carregamento de chave de credenciamento a partir de variável de ambiente

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,15 +1,27 @@
+require('dotenv').config();
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-admin.initializeApp(functions.config().firebase);
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://gitlab-plus.firebaseio.com'
+});
 
 const db = admin.firestore();
 
 exports.receiveIssues = functions.https.onRequest((request, response) => {
-    db.collection('issues').add(request.body);
-
-    response.status(201).send({
-        msg: 'Created',
-        createdAt: Date.now()
-    });
+    db.collection('issues').add(request.body)
+        .then(() => {
+            response.status(201).send({
+                msg: 'Created',
+                createdAt: Date.now()
+            });
+        }).catch(() => {
+            response.status(400).send({
+                msg: 'Not Created'
+            });
+        });
 });
