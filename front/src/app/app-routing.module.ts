089fdedfd1922b85d96f373ab9665057325f102b
PauloRenato98 <paulo__159@hotmail.com>
2020-04-29 23:35:35 -0300

    add routing para pegar o cadastro

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { AuthGuard } from './pages/auth-guard.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
   { path: 'dashboard', canActivate: [AuthGuard], component: DashboardComponent },
   { path: 'issues', canActivate: [AuthGuard], component: IssuesComponent },
   { path: 'burndown', canActivate: [AuthGuard], component: BurndownComponent },
