4361ddd32ac1b2840342a82821cb3aa0f3971eaf
PauloRenato98 <paulo__159@hotmail.com>
2020-05-03 00:07:46 -0300

    #94 - Removendo arquivos errados

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -31,7 +31,7 @@ import { AppFilterIssuesBurndownComponent } from './components/app-filter-issues
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
-import { MensagemErrorComponent } from './mensagem-error/mensagem-error.component';
+
 
 
 
